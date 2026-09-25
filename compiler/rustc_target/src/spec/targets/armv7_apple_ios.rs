b3988c65b3c4d0412d85b753da9f30e511dbb034
JiJingren <140252332+JiJingren@users.noreply.github.com>
2026-09-25 15:37:38 +0800

    Add ARMv7 target support for iOS
    Define target specifications for ARMv7 on iOS.

---
+++ compiler/rustc_target/src/spec/targets/armv7_apple_ios.rs
@@ -0,0 +1,27 @@
+use crate::spec::base::apple::{Arch, TargetEnv, base};
+use crate::spec::{Os, SanitizerSet, Target, TargetMetadata, TargetOptions};
+
+pub(crate) fn target() -> Target {
+    let (mut opts, llvm_target, arch) = base(Os::IOs, Arch::Armv7, TargetEnv::Normal);
+
+    opts.has_elf_tls = false;
+
+    Target {
+        llvm_target,
+        metadata: TargetMetadata {
+            description: Some("ARMv7 iOS".into()),
+            tier: Some(3),
+            host_tools: Some(false),
+            std: Some(true),
+        },
+        pointer_width: 32,
+        data_layout: "e-m:o-p:32:32-Fi8-f64:32:64-v64:32:64-v128:32:128-a:0:32-n32-S32".into(),
+        arch,
+        options: TargetOptions {
+            features: "+v7,+vfp3,+neon".into(),
+            max_atomic_width: Some(64),
+            supported_sanitizers: SanitizerSet::ADDRESS,
+            ..opts
+        },
+    }
+}
